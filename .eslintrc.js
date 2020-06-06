9a5b93d90c50d88d5a092e2e763c49e2f748cab1
Nicolas Belaiche <nicolas.bellaiche@ign.fr>
2020-06-06 16:16:51 +0200

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,24 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 11
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    'global-require': 'off',
+    'no-inner-declarations': 'off',
+  },
+  settings: {
+  },
 };
--- client.js
+++ client.js
@@ -1,61 +1,59 @@
-const electron = require("electron");
-const app = electron.app;
-const path = require("path");
+const electron = require('electron');
 
-const BrowserWindow = electron.BrowserWindow;
+const { app } = electron;
+
+const { BrowserWindow } = electron;
 
 let mainWindow;
-var url=process.env.GPAO_URL
+const url = process.env.GPAO_URL;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({
-        "width": 1200,
-        "height": 1200,
-        "webPreferences": {
-           nodeIntegration: true,
-           webSecurity: false
-        }
-    })
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 1200,
+    webPreferences: {
+      nodeIntegration: true,
+      webSecurity: false,
+    },
+  });
 
-  var args = require('minimist')(process.argv)
-  if (args.hasOwnProperty('ihm')) {
-      const fs = require('fs')
-      let rawdata = fs.readFileSync(args['ihm'])
-      console.log('url', url)
-      if (url == undefined) {
-          console.log('env variable GPAO_URL undefined');
-          process.exit(1)
-      }
-      mainWindow.loadURL(url + '/creation', {
-        postData: [{
-          type: "rawData",
-          bytes: Buffer.from(rawdata)
-        }],
-        extraHeaders: "Content-Type: application/x-www-form-urlencoded; charset=UTF-8"
-      })
-  }
-  else {
-      mainWindow.loadURL( url )
+  const args = require('minimist')(process.argv);
+  if ({}.hasOwnProperty.call(args, 'ihm')) {
+    const fs = require('fs');
+    const rawdata = fs.readFileSync(args.ihm);
+    if (url === undefined) {
+    // eslint-disable-next-line no-console
+      console.log('env variable GPAO_URL undefined');
+      process.exit(1);
+    }
+    mainWindow.loadURL(`${url}/creation`, {
+      postData: [{
+        type: 'rawData',
+        bytes: Buffer.from(rawdata),
+      }],
+      extraHeaders: 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8',
+    });
+  } else {
+    mainWindow.loadURL(url);
   }
 
-  mainWindow.on("closed", () => {
-    mainWindow = null
-  })
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
 }
 
 // set the view engine to ejs
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
-app.on("ready", createWindow);
+app.on('ready', createWindow);
 
-app.on("window-all-closed", () => {
-    app.quit();
-  
-})
+app.on('window-all-closed', () => {
+  app.quit();
+});
 
-app.on("activate", () => {
+app.on('activate', () => {
   if (mainWindow === null) {
     createWindow();
   }
-})
+});
