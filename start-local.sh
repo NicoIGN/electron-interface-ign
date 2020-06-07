ce924ad04b0792f322a90f4ebbe371acf3c55255
Nicolas Belaiche <nicolas.bellaiche@ign.fr>
2020-06-07 17:06:39 +0200

    add validate on start-local

---
+++ start-local.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+BASEDIR=$(dirname "$0")
+cd $BASEDIR
+
+export GPAO_URL='http://localhost:8000'
+
+export DIRECTORY='/Users/nbellaiche/DATA/TEMP/'
+export APPLICATION_DIR='/Applications/MicMacMgrV2.app/Contents/MacOS'
+export IGN_DATA='/Applications/MicMacMgrV2.app/Contents/Data'
+export GDAL_DATA='/Applications/MicMacMgrV2.app/Contents/Data/geodesy/gdal'
+export PROJ_LIB='/Applications/MicMacMgrV2.app/Contents/Data/geodesy/nad'
+export GPAO_CLIENT_DIR='/Users/nbellaiche/DATA/TEMP/ign-gpao-client'
+electron main.js  --ihm ./data/ihm_micmacmgr.json
