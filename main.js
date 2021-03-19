2b8760f633d1bf41eea84307f07c2463e37975dd
Nicolas Bellaiche <nicolas.bellaiche@ign.fr>
2021-03-19 16:53:50 +0100

    adding capability --parameters at initialization stage

--- main.js
+++ main.js
@@ -8,61 +8,81 @@ let mainWindow = null;
 let ihmData = {};
 
 function main() {
-  // Create the new window
-  mainWindow = new BrowserWindow({
+    // Create the new window
+    mainWindow = new BrowserWindow({
     width: 1200,
     height: 1200,
     webPreferences: {
-      nodeIntegration: true,
+    nodeIntegration: true,
     },
-  });
-
-
-  const args = require('minimist')(process.argv);
-
-  if ({}.hasOwnProperty.call(args, 'ihm')) {
-    const fs = require('fs');
-    const rawdata = fs.readFileSync(args.ihm);
-    ihmData = JSON.parse(rawdata);
-
-    ihmData.js_folder = '../../js';
-    ihmData.page = `${ignGpao.viewFolder()}/pages/creation`;
-
-    if ({}.hasOwnProperty.call(ihmData, 'ihm')) {
-      const result = ignGpao.validate(ihmData);
-      if (!result.valid) {
-        const messagelist = [];
-        Array.prototype.forEach.call(result.errors, (error) => {
-          const submessagelist = ignGpao.analyzeError(error, ihmData);
-          Array.prototype.forEach.call(submessagelist, (submessage) => {
-            messagelist.push(submessage);
-          });
-          if (submessagelist.length < 1) messagelist.push(`${error.message} on ${error.property}`);
-        });
-        if (messagelist) dialog.showErrorBox('Error', messagelist[0]);
-        process.exit(1);
-      } else {
-        ejse.data('no_header', 'on');
-        ejse.data('js_folder', '../../js');
-        ejse.data('ihm_data', ihmData.ihm);
-        mainWindow.loadURL(`file://${ignGpao.viewFolder()}/pages/creation.ejs`);
-      }
+    });
+    
+    
+    const args = require('minimist')(process.argv);
+    
+    if ({}.hasOwnProperty.call(args, 'ihm')) {
+        const fs = require('fs');
+        const rawdata = fs.readFileSync(args.ihm);
+        ihmData = JSON.parse(rawdata);
+        
+        ihmData.js_folder = '../../js';
+        ihmData.page = `${ignGpao.viewFolder()}/pages/creation`;
+        
+        if ({}.hasOwnProperty.call(ihmData, 'ihm')) {
+            const result = ignGpao.validate(ihmData);
+            if (!result.valid) {
+                const messagelist = [];
+                Array.prototype.forEach.call(result.errors, (error) => {
+                    const submessagelist = ignGpao.analyzeError(error, ihmData);
+                    Array.prototype.forEach.call(submessagelist, (submessage) => {
+                        messagelist.push(submessage);
+                    });
+                    if (submessagelist.length < 1) messagelist.push(`${error.message} on ${error.property}`);
+                });
+                if (messagelist) dialog.showErrorBox('Error', messagelist[0]);
+                process.exit(1);
+            } else {
+                ejse.data('no_header', 'on');
+                ejse.data('js_folder', '../../js');
+                ejse.data('ihm_data', ihmData.ihm);
+                mainWindow.loadURL(`file://${ignGpao.viewFolder()}/pages/creation.ejs`);
+            }
+        } else {
+            // eslint-disable-next-line  no-console
+            console.log(dialog.showErrorBox('Error', 'invalid json: ihm key not found'));
+            process.exit(1);
+        }
     } else {
-      // eslint-disable-next-line  no-console
-      console.log(dialog.showErrorBox('Error', 'invalid json: ihm key not found'));
-      process.exit(1);
+        // eslint-disable-next-line  no-console
+        console.log('no ihm json in arguments. Use: electron main.js --ihm ihmfile.json');
+        process.exit(1);
     }
-  } else {
-    // eslint-disable-next-line  no-console
-    console.log('no ihm json in arguments. Use: electron main.js --ihm ihmfile.json');
-    process.exit(1);
-  }
+    
+    if ({}.hasOwnProperty.call(args, 'parameters')) {
+        console.log('parameters file: ', args.parameters);
+        process.env['PARAMETERS'] = args.parameters;
+    }
+    
+    
+    // eslint-disable-next-line  no-unused-vars
+    mainWindow.on('close', (event) => {
+        mainWindow = null;
+        process.exit(0);
+    });
+    
+    
+    mainWindow.webContents.on('did-finish-load',()=>{
+        if (process.env.PARAMETERS) {
+            mainWindow.webContents.send('setparameters', process.env.PARAMETERS)
+        }
+        /*let code = `var promise = Promise.resolve(document.getElementById('name').innerHTML);
+        promise.then(data => data)`;
 
-  // eslint-disable-next-line  no-unused-vars
-  mainWindow.on('close', (event) => {
-    mainWindow = null;
-    process.exit(0);
-  });
+        mainWindow.webContents.executeJavaScript(code, true)
+        .then((result) => {
+            console.log(result) // will be your innherhtml
+        })*/
+    });
 }
 
 app.on('ready', main);
