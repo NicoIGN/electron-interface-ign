000e8f9f793b95ad6956cab1270658d030ba1b30
Nicolas Bellaiche <nicolas.bellaiche@ign.fr>
2021-03-21 17:36:08 +0100

    log + paths

--- examples/micmacmgr/macosx-clang/setenv.sh
+++ examples/micmacmgr/macosx-clang/setenv.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 
+#export MICMACMGR_ROOT=/Applications/MicMacMgrV2.app/
+export MICMACMGR_ROOT=/Users/nbellaiche/dev/MicMacMgr/setup/MicMacMgrV2.app/
 
-export APPLICATION_DIR=/Applications/MicMacMgrV2.app/Contents/MacOS
-export IGN_DATA=/Applications/MicMacMgrV2.app/Contents/Data
-export GDAL_DATA=/Applications/MicMacMgrV2.app/Contents/Data/geodesy/gdal
-export PROJ_LIB=/Applications/MicMacMgrV2.app/Contents/Data/geodesy//nad
+export APPLICATION_DIR=$MICMACMGR_ROOT/Contents/MacOS
+export IGN_DATA=$MICMACMGR_ROOT/Contents/Data
+export GDAL_DATA=$IGN_DATA/geodesy/gdal
+export PROJ_LIB=$IGN_DATA/geodesy//nad
 
 export GPAO_CLIENT_DIR=/Users/nbellaiche/dev/ign-gpao-client
 export DIRECTORY=/Users/nbellaiche/dev/tempo
