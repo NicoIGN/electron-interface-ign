da97429eb3e89596c302184e0a7227430722b394
Nicolas Bellaiche <nicolas.bellaiche@ign.fr>
2021-03-26 21:00:20 +0100

    test windows

--- examples/minimal/launch.bat
+++ examples/minimal/launch.bat
@@ -1,16 +1,24 @@
-#!/usr/bin/env bash
+@echo OFF
 
-#finding the current dir
-cd "$(dirname "${BASH_SOURCE[0]}")"
-SCRIPTPATH="`pwd`"
+set SCRIPTPATH=%CD%
+
+cd ..
+cd ..
+set ROOT=%CD%
+echo ROOT=%ROOT%
+cd %SCRIPTPATH%
+
+REM defining requirements
+set SOME_REQUIRED_ENVIRONMENT_VARIABLE=%SCRIPTPATH%
+set SOME_REQUIRED_ENVIRONMENT_VARIABLE2=%SCRIPTPATH%
 
 
-#defining requirements
-export SOME_REQUIRED_ENVIRONMENT_VARIABLE=$SCRIPTPATH
-export SOME_REQUIRED_ENVIRONMENT_VARIABLE2=$SCRIPTPATH
+REM launching electron
+cd %SCRIPTPATH%
 
-#launching electron
-cd $SCRIPTPATH
+if not exist %SCRIPTPATH%\ihm_minimal.json (
+    echo current directory must be where the ihm/json file is located
+    exit 1
+)
 
-electron $SCRIPTPATH/../../index.js \
---ihm $SCRIPTPATH/ihm_basic.json
+electron -r esm %ROOT%\index.js --ihm %SCRIPTPATH%\ihm_minimal.json
