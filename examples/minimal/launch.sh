60371750853d099e73331b00349b0a16fbba0c8c
Nicolas Belaiche <nicolas.bellaiche@ign.fr>
2021-03-30 13:30:00 +0200

    fix example

--- examples/minimal/launch.sh
+++ examples/minimal/launch.sh
@@ -13,5 +13,10 @@ export SOME_REQUIRED_ENVIRONMENT_VARIABLE=$SCRIPTPATH
 export IHMFILE=$SCRIPTPATH/ihm_minimal.json
 export OPEN_METHOD=open
 
+if [ ! -e $IHMFILE ]; then
+    echo current directory must be where the ihm/json file is located
+    exit 1
+fi
+
 #launching electron
 cd $ROOT && npm start
