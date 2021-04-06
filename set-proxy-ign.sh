f3c15c465d3fd99dde022e3781564acb7be74991
Nicolas Bellaiche <nicolas.bellaiche@ign.fr>
2021-04-06 12:51:36 +0200

    nettoyage

--- set-proxy-ign.sh
+++ set-proxy-ign.sh
@@ -1,4 +1,7 @@
+#!/usr/bin/env bash
+
 export http_proxy=http://proxy.ign.fr:3128
 export https_proxy=http://proxy.ign.fr:3128
-npm config rm proxy &&  npm config set proxy $http_proxy
+
+npm config rm proxy && npm config set proxy $http_proxy
 npm config rm https-proxy && npm config set https-proxy $https_proxy
